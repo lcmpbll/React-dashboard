6059161db45a96ddbda86841bcd8ff705951ea01
Liam Cammpbell <liam22campbell@gmail.com>
2023-01-09 17:48:42 -0800

    Create bar scene

---
+++ src/scenes/bar/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import Header from '../../components/Header';
+// import BarChart from '../../components/BarChart';
+
+const Bar = () => {
+  return (
+    <Box m='20px'>
+      <Header title="Bar Chart" subtitle='Simple Bar Chart'/>
+      <Box height='75vh'>
+        <h1>Bar chart coming soon ...</h1>
+      </Box>
+    </Box>
+  );
+};
+
+export default Bar;
